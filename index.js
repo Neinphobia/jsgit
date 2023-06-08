2bed9ef7100e3b2b949017d2e115928b6a15cfd1
Furkan <furkangonulal.dev@gmail.com>
2023-06-08 15:55:33 +0300

    Fetch get request

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ sayHi("Hello ", "World!");
 //git branch -b yenilikler
 //git checkout master 
 //switching..
+//here is yenilikler branch
+
+
+//simple fetch get request 
+let fetchRes = fetch("https://jsonplaceholder.typicode.com/todos/5");
+    fetchRes.then(res =>
+        res.json()).then(data => {
+            console.log(data)
+        })
