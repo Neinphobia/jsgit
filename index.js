788051d4e5e3157c52a43d7f0ef5e7371a7ef931
Furkan <furkangonulal.dev@gmail.com>
2023-06-08 15:21:52 +0300

    Added an example 😎

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const message = {
     id:553
 }
 console.log(message.id);
+function sayHi(a,b){ 
+
+        return console.log(a+b); //No type safety!
+}
+sayHi("Hello ", "World!");
 //git branch -b yenilikler
 //git checkout master 
 //switching..
